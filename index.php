19ddb3da4b810eb92d6900fea94e9367471cdbbe
k15j <kike15j@gmail.com>
2018-04-12 10:52:00 +0200

    index.php

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <?php
-require_once __DIR__.'/includes/config.php';
-require_once __DIR__.'/includes/formLogin.php';
 
-session_start();
-?>
-<!DOCTYPE html>
+require_once __DIR__.'/includes/config.php';
+
+?><!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Ejemplo 6 para incluir token CSRF en los formularios (Seguridad+Detección de doble envío de formularios)</title>
-	</head>
-  <body>
-    <p>Para evitar la advertencia de recarga de formulario, es necesario que tras procesar el formulario, en caso se éxito,
-    se fuerce al navegador a cargar otra URL (mediante una redirección).</p>
-    
-    <p>Toda la gestión del token CRSF está en <code>includes/formlib.php</code>, el resto del código no cambia !.</p>
-    
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/estilo.css') ?>" />
+  <title>Portada</title>
+</head>
+<body>
+<div id="contenedor">
 <?php
-  $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-  if ( $user ) :
+$app->doInclude('comun/cabecera.php');
+$app->doInclude('comun/sidebarIzq.php');
 ?>
-    <h1>Hola <?= $user ?></h1>
-    <a href="logout.php">Logout</a>
+	<div id="contenido">
+		<h1>Página principal</h1>
+		<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+	</div>
 <?php
-  else :
-    formularioLogin();
-  endif;
+$app->doInclude('comun/sidebarDer.php');
+$app->doInclude('comun/pie.php');
 ?>
-  </body>
+</div>
+</body>
 </html>
