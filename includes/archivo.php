52d6a45e7badc927cb455e112b241ee3142e52ba
alvant01 <alvant01@ucm.es>
2018-05-03 11:58:34 +0200

    prototipo archivo.php

---
+++ includes/archivo.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace es\ucm\fdi\aw;
+
+use es\ucm\fdi\aw\Aplicacion as App;
+
+
+class Archivo
+{
+
+	private $id;
+
+	private $nombre;
+
+	private $autor;
+
+	private $destacado;
+
+	private $punt;
+
+	private $ruta;
+
+
+	private function __construct($id, $nombre, $autor, $dest, $puntuacion, $ruta)
+	{
+		$this->id = $id;
+		$this->nombre = $nombre;
+		$this->autor = $autor;
+		$this->destacado = $dest;
+		$this->punt = $puntuacion;
+		$this->ruta = $ruta;
+	}
+
+
+		public static function buscarImagenDest($idAutor)
+	{
+		$app = App::getSingleton();
+   		$conn = $app->conexionBd();
+    	$query = sprintf("SELECT * FROM archivo A WHERE A.autor='%s'AND A.imgDest = 1", $conn->real_escape_string($idAutor));
+		$rs = $conn->query($query);
+
+
+
+    	if ($rs && $rs->num_rows == 1) 
+    	{
+    		$fila = $rs->fetch_assoc();
+      		$img = new archivo($fila['id'], $fila['nombre'], $fila['autor'], $fila['imgDest'], $fila['punt'], $fila['ruta']);
+      		$rs->free();
+
+      		return $img;
+    	}
+    	return false;
+	}
+
+
+	public function ruta()
+  	{
+  	  return $this->ruta;
+ 	}
+
+
+
+}
+
+
+?>
