39ff690c77e32b6a9917979486ec760d0c092b6b
K15J <kike15j@gmail.com>
2018-05-09 10:10:59 +0200

    music and video sample

--- musica.php
+++ musica.php
@@ -16,6 +16,15 @@ $app->doInclude('comun/cabecera.php');
 $app->doInclude('comun/sidebarIzq.php');
 ?>
 	<div id="contenido">
+
+		<!- Reference to : https://www.w3schools.com/html/html5_audio.asp->
+		<p> Audio Test: </p>
+		<audio controls>
+		  <source src="uploads/horse.ogg" type="audio/ogg">
+		  <source src="uploads/horse.mp3" type="audio/mpeg">
+		Your browser does not support the audio element.
+		</audio>
+		
 	</div>
 <?php
 $app->doInclude('comun/pie.php');
