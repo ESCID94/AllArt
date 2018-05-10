b2806bbe3e4e7477fc8df842a7d952527f0301ce
K15J <kike15j@gmail.com>
2018-05-10 10:39:12 +0200

    Add files via upload

--- pintura.php
+++ pintura.php
@@ -8,6 +8,7 @@ require_once __DIR__.'/includes/config.php';
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/style.css') ?>" />
   <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/muestra.css') ?>" />
+  <link rel="stylesheet" type="text/css" href="<?= $app->resuelve('/css/imagegrid.css') ?>" />
   <title>*Art</title>
 </head>
 <body>
@@ -17,6 +18,18 @@ $app->doInclude('comun/cabecera.php');
 $app->doInclude('comun/sidebarIzq.php');
 ?>
 	<div id="contenido">
+
+
+		<!-- Image Grid from https://www.w3schools.com/howto/howto_js_image_grid.asp--> 
+
+	<script src="javascript/imagegrid.js" type="text/javascript"></script> 
+	 <h1>Image Grid</h1>
+  	<p>Click on the buttons to change the grid view.</p>
+ 	 <button class="btn" onclick="one()">1</button>
+ 	 <button class="btn active" onclick="two()">2</button>
+	 <button class="btn" onclick="four()">4</button>	
+
+	
 		<div class="row">
   <div class="column">
     <img src="img/pintura.jpg" onclick="openModal();currentSlide(1)" class="hover-shadow">
@@ -71,6 +84,8 @@ $app->doInclude('comun/sidebarIzq.php');
     <div class="column">
       <img class="demo" src="img/pintura3.jpg" onclick="currentSlide(1)" alt="pintura3">
     </div>
+
+
 
 
 
