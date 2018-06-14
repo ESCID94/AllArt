ec7a1e93303e40b0a1c3dabe4772743134e9bda2
caramat <carlos8396@gmail.com>
2018-06-14 20:15:14 +0200

    Eliminado sidebarIzq de Perfil.php

--- Perfil.php
+++ Perfil.php
@@ -13,7 +13,6 @@ require_once __DIR__.'/includes/config.php';
 <div id="contenedor">
 <?php
 $app->doInclude('comun/cabecera.php');
-$app->doInclude('comun/sidebarIzq.php');
 ?>
 	<div id="contenido">
     	<?php		
